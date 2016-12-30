2319527303f1dbc8368b442ce49a967a025447bd
moigoo <moigonn@gmail.com>
2016-12-31 06:57:18 +0900

    追従カメラを追加 ふね、大砲アセットを適応

---
+++ Assets/Script/CharCamera.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharCamera : MonoBehaviour {
+    //http://tech.pjin.jp/blog/2016/11/04/unity_skill_5/
+    GameObject targetObj;
+    Vector3 targetPos;
+    //Vector3 targetRote;
+
+    // Use this for initialization
+    void Start () {
+        targetObj = GameObject.Find("Boat");
+        targetPos = targetObj.transform.position;
+        //targetRote = targetObj.transform.Rotate;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // targetの移動量分、自分（カメラ）も移動する
+        transform.position += targetObj.transform.position - targetPos;
+        targetPos = targetObj.transform.position;
+
+        // マウスの右クリックを押している間
+        //if (Input.GetMouseButton(1))
+        //{
+            // マウスの移動量
+            float mouseInputX = Input.GetAxis("Mouse X");
+            // targetの位置のY軸を中心に、回転（公転）する
+            transform.RotateAround(targetPos, Vector3.up, mouseInputX * Time.deltaTime * 200f);
+        //}
+    }
+}
---
+++ Assets/Script/Gun.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour {
+
+    GameObject targetObj;
+    //Vector3 targetPos;
+    Quaternion targetRote;
+
+    // Use this for initialization
+    void Start () {
+        targetObj = GameObject.Find("CharCamera");
+        //targetPos = targetObj.transform.position;
+        targetRote = targetObj.transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        //transform.rotation =   targetRote;
+        //transform.rotation = Quaternion.FromToRotation(targetObj.transform.up, transform.transform.up);
+        var diff = (targetObj.transform.position - transform.position).normalized;
+        transform.rotation = Quaternion.FromToRotation(Vector3.back, diff);
+    }
+}
