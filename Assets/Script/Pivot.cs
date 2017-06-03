c5f740abefd63036da245a15028e1de3c890e40c
kuwabata <kuwabata@mbi.cs.tut.ac.jp>
2017-06-03 20:50:45 +0900

    砲台の上下傾き機能追加

--- Assets/Script/Pivot.cs
+++ Assets/Script/Pivot.cs
@@ -2,56 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Gun : MonoBehaviour {
+public class Pivot : MonoBehaviour {
 
     GameObject targetObj;
     //Vector3 targetPos;
     Quaternion targetRote;
 
-    GameObject pivotObj;
-
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         targetObj = GameObject.Find("CharCamera");
         //targetPos = targetObj.transform.position;
         targetRote = new Quaternion(0, 0, 0, targetObj.transform.rotation.w);
     }
 
     // Update is called once per frame
-    void Update () {
+    void Update()
+    {
 
         ///マウスの横移動で傾きを変える
         //transform.rotation =   targetRote;
         //transform.rotation = Quaternion.FromToRotation(targetObj.transform.up, transform.transform.up);
         var diff = (targetObj.transform.position - transform.position).normalized;
         var diffy = targetObj.transform.eulerAngles.y - transform.eulerAngles.y;
-        var dy = Mathf.DeltaAngle(targetObj.transform.eulerAngles.y , transform.eulerAngles.y);
+        var dy = Mathf.DeltaAngle(targetObj.transform.eulerAngles.y, transform.eulerAngles.y);
         //transform.Rotate(new Vector3(transform.eulerAngles.x, targetObj.transform.eulerAngles.y, transform.eulerAngles.z));
         //transform.rotation = Quaternion.FromToRotation(Vector3.back, diff);
-
-        //transform.Rotate(new Vector3(0,1,0), diffy);
-        pivotObj = GameObject.Find("Gunpivot");
-        transform.RotateAround(pivotObj.transform.position, new Vector3(0, 1, 0), diffy);
-
-       Debug.Log("dy="+ transform.eulerAngles.x);
+        transform.Rotate(new Vector3(0, 1, 0), diffy);
+        Debug.Log("dy=" + diffy);
         //transform.Rotate(new Vector3(0f, dy, 0f));
         Debug.Log(transform.eulerAngles.x);
-        if (315 < transform.eulerAngles.x || 1> transform.eulerAngles.x)
+        //if (270 < transform.eulerAngles.x )
+        //{
+        if (Input.GetKey(KeyCode.Q))
         {
-            if (Input.GetKey(KeyCode.Q))
-            {
-                transform.Rotate(new Vector3(-1f, 0f, 0f));
+            transform.Rotate(new Vector3(-1f, 0f, 0f));
 
-            }
         }
-        if (359 > transform.eulerAngles.x  && 270 < transform.eulerAngles.x)
+        //}
+        //if (270 < transform.eulerAngles.x)
+        //{
+        if (Input.GetKey(KeyCode.R))
         {
-            if (Input.GetKey(KeyCode.R))
-            {
-                transform.Rotate(new Vector3(1f, 0f, 0f));
+            transform.Rotate(new Vector3(1f, 0f, 0f));
 
-            }
         }
+        //}
 
 
     }
